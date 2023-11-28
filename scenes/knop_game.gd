f76dd29f6b02c943a34020100d4b164045576ae7
hugipugi <17346@waerdenborch.nl>
2023-11-28 15:11:24 +0100

    knop in game

---
+++ scenes/knop_game.gd
@@ -0,0 +1,7 @@
+extends TextureButton
+
+
+
+func _pressed():
+	get_tree().change_scene_to_file("res://scenes/instellingen_game.tscn");
+	
--- scenes/singleplayer.gd
+++ scenes/singleplayer.gd
@@ -3,4 +3,4 @@ extends TextureButton
 
 
 func _pressed():
-	get_tree().change_scene_to_file("res://scenes/game.tscn");
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
