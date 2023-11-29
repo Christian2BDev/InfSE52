63a45d2f3dcd5381dbd81c5a9752e97e8e65e2ff
hugipugi <17346@waerdenborch.nl>
2023-11-29 11:15:46 +0100

    knopupdate

--- scenes/knop_game.gd
+++ scenes/knop_game.gd
@@ -3,5 +3,7 @@ extends TextureButton
 
 
 func _pressed():
-	get_tree().change_scene_to_file("res://scenes/instellingen_game.tscn");
+	
+	var inst =preload("res://scenes/instellingen_game.tscn").instance()
+	get_tree().root.add_child(inst)
 	
