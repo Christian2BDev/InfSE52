12fb69fd55d73154fa1ef26b1650a2caf9624f31
Christian2BDev <christiantenbrinke@hotmail.com>
2023-12-13 15:57:02 +0100

    vollidion

---
+++ scenes/ball.gd
@@ -0,0 +1,20 @@
+extends RigidBody3D
+
+
+func _on_body_entered(body):
+	if body.is_in_group("p1"):
+		print("p1 scored")
+		score("p1label")
+	if body.is_in_group("p2"):
+		print("p2 scored")
+		score("p2label")
+		
+
+func score(who):
+	get_node("../UI/UIControl/64/" + who).text = str(int(get_node("../UI/UIControl/64/" + who).text) + 1)
+	
+	var punt= get_node("../UI/UIControl/64/" + who).text
+	
+	if punt == 11: 
+		pass
+
