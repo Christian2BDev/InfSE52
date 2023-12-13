b08ecdc8826f3ab43be0cb716b45d9a37723d579
Christian2BDev <christiantenbrinke@hotmail.com>
2023-12-13 22:59:44 +0100

    game basis

--- scenes/ball.gd
+++ scenes/ball.gd
@@ -1,18 +1,24 @@
 extends RigidBody3D
-
-
+var old_last
+var last
 func _on_body_entered(body):
-	if body.is_in_group("p1"):
-		print("p1 scored")
-		score("p1label")
-	if body.is_in_group("p2"):
-		print("p2 scored")
-		score("p2label")
-		
-
+	old_last = last
+	last = body.name
+	if body.is_in_group("table"):
+		if last == old_last:
+			score(body.name+"label")
+			reset()
+	if body.is_in_group("bound"):
+		reset()
 func score(who):
 	get_node("../UI/UIControl/64/" + who).text = str(int(get_node("../UI/UIControl/64/" + who).text) + 1)
 	
-	var punt= get_node("../UI/UIControl/64/" + who).text
-	
+	var punt= int(get_node("../UI/UIControl/64/" + who).text)
+	if punt >=11:
+		get_tree().change_scene_to_file("res://scenes/gameover.tscn");
 
+func reset():
+	position = Vector3(0,17.287,15.702)
+	linear_velocity = Vector3(0,0,0)
+	old_last =""
+	last = ""
--- scenes/batje2.gd
+++ scenes/batje2.gd
@@ -9,7 +9,7 @@ func _ready():
 
 func _physics_process(delta):
 	if ball.position.z <=0:
-		position = position.move_toward(Vector3(ball.position.x, ball.position.y,ball.position.z + 2),0.1)
+		position = position.move_toward(Vector3(ball.position.x, ball.position.y,ball.position.z + 2),0.3)
 	else:
 		position = position.move_toward(Vector3(ball.position.x, ball.position.y,-17),0.05)
 		
